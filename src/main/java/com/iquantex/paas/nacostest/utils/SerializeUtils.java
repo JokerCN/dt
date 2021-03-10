910f621104237e5298bb5cb168d4722bc52a1775
david.yang <yiwei.yang@iquantex.com>
2021-03-10 10:35:12 +0800

    feat(strategy): implement APStrategy.testExist

--- src/main/java/com/iquantex/paas/nacostest/utils/SerializeUtils.java
+++ src/main/java/com/iquantex/paas/nacostest/utils/SerializeUtils.java
@@ -1,7 +1,12 @@
 package com.iquantex.paas.nacostest.utils;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.iquantex.paas.nacostest.exceptions.NacosTestException;
 import lombok.SneakyThrows;
+
+import java.io.IOException;
 
 public class SerializeUtils {
 
@@ -17,4 +22,22 @@ public class SerializeUtils {
         return objectMapper.readValue(json, clazz);
     }
 
+    public static ObjectNode createEmptyJsonNode() {
+        return new ObjectNode(objectMapper.getNodeFactory());
+    }
+
+    /**
+     * Json string deserialize to Jackson {@link JsonNode}.
+     *
+     * @param json json string
+     * @return {@link JsonNode}
+     * @throws com.iquantex.paas.nacostest.exceptions.NacosTestException if deserialize failed
+     */
+    public static JsonNode toObj(String json) {
+        try {
+            return objectMapper.readTree(json);
+        } catch (IOException e) {
+            throw new NacosTestException(e);
+        }
+    }
 }
--- src/test/java/com/iquantex/paas/nacostest/service/strategy/APStrategyTest.java
+++ src/test/java/com/iquantex/paas/nacostest/service/strategy/APStrategyTest.java
@@ -9,6 +9,7 @@ import com.iquantex.paas.nacostest.utils.RequestSender;
 import com.iquantex.paas.nacostest.utils.Response;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.Assert;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -16,6 +17,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.Assert.*;
@@ -55,6 +58,21 @@ public class APStrategyTest {
         Request request = apStrategy.makeListInstanceRequest("127.0.0.1");
         Response response = requestSender.sendRequest(request).get();
         log.info("> list instance : {}", response.getResponseEntity().getBody());
+    }
+
+    @Test
+    public void testExist() {
+        String body = "{\"hosts\":[{\"ip\":\"200.200.200.200\",\"port\":8080,\"valid\":true,\"healthy\":true,\"marked\":false,\"instanceId\":\"20.18.7.10#8080#DEFAULT#DEFAULT_GROUP@@nacos.naming.serviceName\",\"metadata\":{},\"enabled\":true,\"weight\":1.0,\"clusterName\":\"DEFAULT\",\"serviceName\":\"nacos.naming.serviceName\",\"ephemeral\":true}],\"dom\":\"nacos.naming.serviceName\",\"name\":\"DEFAULT_GROUP@@nacos.naming.serviceName\",\"cacheMillis\":3000,\"lastRefTime\":1615342792626,\"checksum\":\"0f579c87395fb94ff691d287b4d67cee\",\"useSpecifiedURL\":false,\"clusters\":\"\",\"env\":\"\",\"metadata\":{}}";
+        Response response = new Response(new ResponseEntity<>(body, HttpStatus.OK));
+        Assert.assertTrue(apStrategy.testExist(response));
+
+        String bodyFalse = "{\"hosts\":[{\"ip\":\"200.200.200.201\",\"port\":8080,\"valid\":true,\"healthy\":true,\"marked\":false,\"instanceId\":\"20.18.7.10#8080#DEFAULT#DEFAULT_GROUP@@nacos.naming.serviceName\",\"metadata\":{},\"enabled\":true,\"weight\":1.0,\"clusterName\":\"DEFAULT\",\"serviceName\":\"nacos.naming.serviceName\",\"ephemeral\":true}],\"dom\":\"nacos.naming.serviceName\",\"name\":\"DEFAULT_GROUP@@nacos.naming.serviceName\",\"cacheMillis\":3000,\"lastRefTime\":1615342792626,\"checksum\":\"0f579c87395fb94ff691d287b4d67cee\",\"useSpecifiedURL\":false,\"clusters\":\"\",\"env\":\"\",\"metadata\":{}}";
+        response.setResponseEntity(new ResponseEntity<>(bodyFalse, HttpStatus.OK));
+        Assert.assertFalse(apStrategy.testExist(response));
+
+        String bodyFalse2 = "{\"name\":\"DEFAULT_GROUP@@nacos.naming.serviceName1\",\"clusters\":\"\",\"cacheMillis\":3000,\"hosts\":[]}";
+        response.setResponseEntity(new ResponseEntity<>(bodyFalse2, HttpStatus.OK));
+        Assert.assertFalse(apStrategy.testExist(response));
     }
 
     @Configuration
