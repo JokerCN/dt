e0e3c1d399d67ce3ab880fd1435f666fd294a9ee
david.yang <yiwei.yang@iquantex.com>
2021-03-10 10:58:21 +0800

    feat(strategy): implement Response.toString

---
+++ src/test/java/com/iquantex/paas/nacostest/utils/ResponseTest.java
@@ -0,0 +1,11 @@
+package com.iquantex.paas.nacostest.utils;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+@Slf4j
+public class ResponseTest {
+
+}
